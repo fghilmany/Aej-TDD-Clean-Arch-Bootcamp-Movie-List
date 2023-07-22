7765681ba5fad0cc83a7da8b9d48ffe1bf50138c
fghlmay62 <fghilmany62@gmail.com>
2023-07-22 21:00:57 +0700

    Create MainViewModel

--- app/src/main/java/com/fghilmany/movielist/di/AppModule.kt
+++ app/src/main/java/com/fghilmany/movielist/di/AppModule.kt
@@ -1,7 +1,9 @@
 package com.fghilmany.movielist.di
 
+import com.fghilmany.movielist.ui.main.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-
+    viewModel { MainViewModel(get()) }
 }
---
+++ app/src/main/java/com/fghilmany/movielist/ui/main/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.fghilmany.movielist.ui.main
+
+import androidx.lifecycle.ViewModel
+import com.fghilmany.movielist.core.data.MovieRepository
+
+
+class MainViewModel(private val movieRepository: MovieRepository): ViewModel() {
+}
