da75cbb15e2f6ee9e584fc599edda82dd10aadd0
fghlmay62 <fghilmany62@gmail.com>
2023-07-22 21:47:48 +0700

    Create get movie function in MainViewModel

--- app/src/main/java/com/fghilmany/movielist/ui/main/MainViewModel.kt
+++ app/src/main/java/com/fghilmany/movielist/ui/main/MainViewModel.kt
@@ -1,8 +1,10 @@
 package com.fghilmany.movielist.ui.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.fghilmany.movielist.core.data.MovieRepository
 
 
 class MainViewModel(private val movieRepository: MovieRepository): ViewModel() {
+    fun getMovie() = movieRepository.getMovie().asLiveData()
 }
