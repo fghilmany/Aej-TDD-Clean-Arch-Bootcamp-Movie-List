7d2cc7f3321901c9a8b45024e81811823f263728
fghlmay62 <fghilmany62@gmail.com>
2023-07-22 17:14:10 +0700

    Init koin in Application

---
+++ app/src/main/java/com/fghilmany/movielist/MyApplication.kt
@@ -0,0 +1,25 @@
+package com.fghilmany.movielist
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import timber.log.Timber
+
+class MyApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+
+        startKoin {
+            androidLogger()
+            androidContext(this@MyApplication)
+            modules(
+                listOf(
+
+                )
+            )
+        }
+    }
+}
