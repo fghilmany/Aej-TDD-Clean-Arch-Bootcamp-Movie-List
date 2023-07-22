bfd6b4936f0848e75b7b0ab10e3f4f50695cd667
fghlmay62 <fghilmany62@gmail.com>
2023-07-22 19:08:08 +0700

    Create RepositoryLoader

---
+++ app/src/main/java/com/fghilmany/movielist/core/data/RepositoryLoader.kt
@@ -0,0 +1,29 @@
+package com.fghilmany.movielist.core.data
+
+import com.fghilmany.movielist.core.data.source.remote.network.ApiResponse
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import timber.log.Timber
+
+abstract class RepositoryLoader<RequestType> {
+    private var result: Flow<Resource<RequestType>> = flow {
+
+        emit(Resource.Loading())
+        when (val apiResponse = createCall().first()) {
+            is ApiResponse.Success -> {
+                emit(Resource.Success(apiResponse.data))
+            }
+            is ApiResponse.Empty -> {}
+            is ApiResponse.Error -> {
+                emit(Resource.Error(apiResponse.errorMessage))
+                Timber.e(apiResponse.errorMessage)
+            }
+        }
+
+    }
+
+    protected abstract suspend fun createCall(): Flow<ApiResponse<RequestType>>
+
+    fun asFlow(): Flow<Resource<RequestType>> = result
+}
