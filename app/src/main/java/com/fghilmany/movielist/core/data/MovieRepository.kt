1e202db5bfa49f826de03e766c47d9cbe62af7ed
fghlmay62 <fghilmany62@gmail.com>
2023-07-22 17:49:04 +0700

    Create MovieRepository

---
+++ app/src/main/java/com/fghilmany/movielist/core/data/MovieRepository.kt
@@ -0,0 +1,8 @@
+package com.fghilmany.movielist.core.data
+
+import com.fghilmany.movielist.core.data.source.remote.RemoteDataSource
+
+class MovieRepository(
+    private val remoteDataSource: RemoteDataSource
+) {
+}
