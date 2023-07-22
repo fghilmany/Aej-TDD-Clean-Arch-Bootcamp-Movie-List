5f90ed76db7deb870255b8e6a96245e825ecbe11
fghlmay62 <fghilmany62@gmail.com>
2023-07-22 17:23:15 +0700

    Create MovieService

--- app/src/main/java/com/fghilmany/movielist/core/data/source/remote/network/MovieService.kt
+++ app/src/main/java/com/fghilmany/movielist/core/data/source/remote/network/MovieService.kt
@@ -1,4 +1,9 @@
 package com.fghilmany.movielist.core.data.source.remote.network
 
+import com.fghilmany.movielist.core.data.source.remote.response.MovieResponse
+import retrofit2.http.GET
+
 interface MovieService {
+    @GET("discover/movie")
+    fun getMovies(): MovieResponse
 }
