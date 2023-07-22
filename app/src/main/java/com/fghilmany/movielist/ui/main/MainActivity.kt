70f333678065b1f37b0db2710cc24963bba126f9
fghlmay62 <fghilmany62@gmail.com>
2023-07-22 23:12:34 +0700

    Refactor to view binding

--- app/src/main/java/com/fghilmany/movielist/ui/main/MainActivity.kt
+++ app/src/main/java/com/fghilmany/movielist/ui/main/MainActivity.kt
@@ -2,11 +2,13 @@ package com.fghilmany.movielist.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.fghilmany.movielist.R
+import com.fghilmany.movielist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
