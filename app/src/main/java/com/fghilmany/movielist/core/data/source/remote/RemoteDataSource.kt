818a89029e8c46951a9420b76767ca01aefcac9e
fghlmay62 <fghilmany62@gmail.com>
2023-07-22 17:39:06 +0700

    Create RemoteDataSource

---
+++ app/src/main/java/com/fghilmany/movielist/core/data/source/remote/RemoteDataSource.kt
@@ -0,0 +1,6 @@
+package com.fghilmany.movielist.core.data.source.remote
+
+import com.fghilmany.movielist.core.data.source.remote.network.MovieService
+
+class RemoteDataSource(private val movieService: MovieService) {
+}
