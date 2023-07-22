7fbbd65f8616e0fe48b52600108fd17ddb833210
fghlmay62 <fghilmany62@gmail.com>
2023-07-22 23:13:52 +0700

    Inject MainViewModel to MainActivity

--- app/src/main/java/com/fghilmany/movielist/ui/main/MainActivity.kt
+++ app/src/main/java/com/fghilmany/movielist/ui/main/MainActivity.kt
@@ -3,10 +3,13 @@ package com.fghilmany.movielist.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fghilmany.movielist.databinding.ActivityMainBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private val viewModel: MainViewModel by viewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
